04e101d7b00b1f1c0a9ec84c7ea96b29d1955251
Ralph Loader <suckfish@ihug.co.nz>
2012-10-24 10:08:37 +1300

    Seems to get correct output.

--- core.vhd
+++ core.vhd
@@ -28,7 +28,7 @@ architecture behavioral of core is
   end;
   function quad (i : natural; x : dataset_t) return word_t is
   begin
-    return L (x(i-3) xor x(i-8) xor x(i-10) xor x(i-14));
+    return L (x(i-3) xor x(i-8) xor x(i-14) xor x(i-16));
   end;
 
   signal Ea : dataset_t (12 to 27);
@@ -44,7 +44,6 @@ architecture behavioral of core is
 
   signal R : dataset_t (0 to 84);
   signal W : dataset_t (0 to 79);
-  signal bypass : words_5_t;
 
   signal oB : word_t;
   signal oC1 : word_t;
@@ -120,30 +119,24 @@ begin
                 R(i+4), R(i+3), R(i+2), R(i+1), R(i), clk);
   end generate;
 
-  bypasses: for i in 0 to 4 generate
-    byp : entity work.delay generic map (102 + (i-1) / dly)
-      port map (input (i), bypass (i), clk);
-  end generate;
-
   process
   begin
     wait until rising_edge(clk);
 
-    -- FIXME - this is not right.
-    output(0) <= R(84) + bypass(0);
+    output(0) <= R(84) + x"67452301";
     oB <= R(83);
-    output(1) <= oB + bypass(1);
+    output(1) <= oB + x"efcdab89";
     oC1 <= R(82) rol 30;
     oC2 <= oC1;
-    output(2) <= oC2 + bypass(2);
+    output(2) <= oC2 + x"98badcfe";
     oD1 <= R(81) rol 30;
     oD2 <= oD1;
     oD3 <= oD2;
-    output(3) <= oD3 + bypass(3);
+    output(3) <= oD3 + x"10325476";
     oE1 <= R(80) rol 30;
     oE2 <= oE1;
     oE3 <= oE2;
     oE4 <= oE3;
-    output(4) <= oE4 + bypass(4);
+    output(4) <= oE4 + x"c3d2e1f0";
   end process;
 end behavioral;
