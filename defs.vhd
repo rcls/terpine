2f782b0d280580e343f7d8d4848c5dbbcf3a0478
Ralph Loader <suckfish@ihug.co.nz>
2012-10-24 20:41:34 +1300

    Gets timing closure at 5ns; either with a 3-way adder or without.

--- defs.vhd
+++ defs.vhd
@@ -10,6 +10,6 @@ package defs is
   subtype words_16_t is dataset_t (0 to 15);
   subtype words_5_t is dataset_t (0 to 4);
 
-  constant dly : integer := 8;
+  constant dly : integer := 5;
 
 end defs;
